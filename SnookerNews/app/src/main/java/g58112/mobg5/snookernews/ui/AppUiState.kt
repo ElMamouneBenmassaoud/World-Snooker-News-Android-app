eff97e9df7808167ac3fabd4d43a326323dc6a77
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-11-19 04:53:36 +0100

    add a state for password wrong or not

--- SnookerNews/app/src/main/java/g58112/mobg5/snookernews/ui/AppUiState.kt
+++ SnookerNews/app/src/main/java/g58112/mobg5/snookernews/ui/AppUiState.kt
@@ -3,4 +3,5 @@ package g58112.mobg5.snookernews.ui
 data class AppUiState(
     val isEmailValidationAsked: Boolean = false,
     val isMailWrong: Boolean = true,
+    val isPasswordWrong: Boolean = true
 )
